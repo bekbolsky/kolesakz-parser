d3766b9ffba3c00a74f8b6259de39ebbafa5d38d
Bekbolat Kuralbayev <bkb@live.ru>
2021-04-02 02:26:51 +0600

    `refactor`: yet another readability improvements

--- parser.py
+++ parser.py
@@ -2,15 +2,17 @@ import csv
 import re
 from random import uniform
 import time
+from typing import List, Tuple
 
 
 import requests
 from bs4 import BeautifulSoup
-from bs4.element import ResultSet
+from bs4.element import ResultSet, Tag
 from tqdm import trange
 
 
-URL = "https://kolesa.kz/spectehnika/gruzoviki/samosval/region-zhambilskaya-oblast/"
+BASE_URL = "https://kolesa.kz"
+URL = f"{BASE_URL}/spectehnika/gruzoviki/region-zhambilskaya-oblast/"
 HEADERS = {
     "accept": "*/*",
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
@@ -32,7 +34,8 @@ def get_html(url: str) -> str:
         [str]: [HTML page from the request]
     """
     response = requests.get(url, headers=HEADERS)
-    return response.text
+    html = response.text
+    return html
 
 
 def pages_count(html: str) -> int:
@@ -46,74 +49,69 @@ def pages_count(html: str) -> int:
     return last_page
 
 
-def gather_data(adverts: ResultSet) -> list:
-    data = []
+def gather_valuable_data(advert: Tag) -> Tuple[str, ...]:
     engine_volume_pattern = re.compile(r"(^\d+.*)(\s)(л$)")
+    fuels = ("бензин", "дизель", "газ-бензин", "газ", "гибрид", "электричество")
+
+    vehicle_mark = " ".join(
+        advert.find("span", {"class": "a-el-info-title"}).text.split()[:3]
+    )
+    price = "".join(advert.find("span", {"class": "price"}).text.split()[:-1])
+    emergency = advert.find("span", {"class": "emergency"}).text.split(",")[0]
+    description = (
+        advert.find("div", {"class": "a-search-description"})
+        .text.strip()
+        .split(",")[:6]
+    )
+    year = description[0].strip()
+
+    if (
+        description[1].strip() not in fuels
+        and re.match(engine_volume_pattern, description[1].strip()) is None
+    ):
+        vehicle_type = description[1].strip()
+    else:
+        vehicle_type = ""
+
+    if re.match(engine_volume_pattern, description[2].strip()):
+        engine_volume = description[2].strip()
+    elif re.match(engine_volume_pattern, description[1].strip()):
+        engine_volume = description[1].strip()
+    else:
+        engine_volume = ""
+
+    fuel_type = ""
+    for target in description[1:]:
+        if target.strip() in fuels:
+            fuel_type = target.strip()
+
+    data = (
+        vehicle_mark,
+        year,
+        price,
+        emergency,
+        fuel_type,
+        engine_volume,
+        vehicle_type,
+    )
+
+    return data
+
+
+def collect_data(adverts: ResultSet) -> List:
+    collection = []
     for ad in adverts:
         try:
-            vehicle_mark = " ".join(
-                ad.find("span", {"class": "a-el-info-title"}).text.split()[:3]
-            )
-            price = "".join(ad.find("span", {"class": "price"}).text.split()[:-1])
-            emergency = ad.find("span", {"class": "emergency"}).text.split(",")[0]
-            description = (
-                ad.find("div", {"class": "a-search-description"})
-                .text.strip()
-                .split(",")[:6]
-            )
-            year = description[0].strip()
-            vehicle_type = description[1].strip()
-            engine_volume = (
-                description[2].strip()
-                if re.match(engine_volume_pattern, description[2].strip())
-                else ""
-            )
-            if engine_volume != "":
-                fuel_type = description[3].strip()
-            elif emergency == description[2].strip():
-                fuel_type = ""
-            elif len(description[2].strip()) > 13:
-                fuel_type = ""
-            else:
-                fuel_type = description[2].strip()
-            data.append(
-                [
-                    vehicle_mark,
-                    year,
-                    price,
-                    emergency,
-                    fuel_type,
-                    engine_volume,
-                    vehicle_type,
-                ]
-            )
+            data = gather_valuable_data(ad)
+            collection.append(data)
         except AttributeError:
             continue
         except IndexError:
             continue
-    return data
+    return collection
 
 
-last_page = pages_count(get_html(URL))
-
-data_collection = []
-
-for i in trange(1, last_page + 1, desc="Progress"):
-
-    site_response = get_html(f"{URL}?page={i}")
-    soup_object = BeautifulSoup(site_response, "lxml")
-    # print(f"Page parsing: {i} of {last_page}. Amount remaining: {last_page-i}")
-
-    ads_list = soup_object.find_all("div", {"class": "a-elem"})
-    data = gather_data(ads_list)
-    data_collection.extend(data)
-
-    rand_sec = round(uniform(0.4, 1), 2)
-    # print(f"sleeping for {rand_sec} secs")
-    time.sleep(rand_sec)
-
-
-def save_data(filename, data: list):
+def save_data(filename, data: List) -> None:
     with open(filename, "wt", encoding="utf-8") as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(
@@ -132,4 +130,21 @@ def save_data(filename, data: list):
             writer.writerow(row)
 
 
-save_data("data.csv", data_collection)
+def main():
+    last_page = pages_count(get_html(URL))
+
+    data_collection = []
+    for i in trange(1, last_page + 1, desc="Progress"):
+        html = get_html(f"{URL}?page={i}")
+        soup = BeautifulSoup(html, "lxml")
+        ads_list = soup.find_all("div", {"class": "a-elem"})
+        data = collect_data(ads_list)
+        data_collection.extend(data)
+        rand_sec = round(uniform(0.4, 1), 2)
+        time.sleep(rand_sec)
+
+    save_data("pretty_data.csv", data_collection)
+
+
+if __name__ == "__main__":
+    main()
